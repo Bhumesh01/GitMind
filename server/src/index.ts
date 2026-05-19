2f53840039245b51e576c68ec37f1031a6a91add
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2026-05-19 23:44:49 +0530

    basic structure+cloning logic

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World");
+import express from "express";
+import { router } from "./routes/routes.js";
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res)=>{
+    res.status(200).json({
+        "message": "Success"
+    })
+})
+app.use("/api/v1", router)
+app.listen(3000, ()=>{
+    console.log("Server running on PORT 3000");
+})
---
+++ server/src/routes/analyzeRepo.ts
@@ -0,0 +1,37 @@
+import { Router } from "express";
+import { simpleGit } from "simple-git";
+import type { SimpleGit } from "simple-git";
+
+const repoRouter = Router();
+
+repoRouter.post("/", async (req, res) => {
+    const { repoUrl } = req.body;
+
+    if (!repoUrl) {
+        return res.status(400).json({
+            message: "Invalid repo URL",
+        });
+    }
+
+    const git: SimpleGit = simpleGit();
+
+    const repoPath = `./temp/${Date.now()}`;
+
+    try {
+        await git.clone(repoUrl, repoPath, ["--depth", "1"]);
+
+        return res.status(200).json({
+            message: "Repository cloned successfully",
+            path: repoPath,
+        });
+
+    } catch (err) {
+        console.error(err);
+
+        return res.status(500).json({
+            message: "Failed to clone repository",
+        });
+    }
+});
+
+export default repoRouter;
